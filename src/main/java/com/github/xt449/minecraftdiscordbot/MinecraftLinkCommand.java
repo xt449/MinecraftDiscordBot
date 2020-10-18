29b25337caacc6a6db4fc013180e18f2db5c536c
xt449 <alpha.xt449.omega@gmail.com>
2020-10-18 11:54:39 -0400

    Harmless error fix

--- src/main/java/com/github/xt449/minecraftdiscordbot/MinecraftLinkCommand.java
+++ src/main/java/com/github/xt449/minecraftdiscordbot/MinecraftLinkCommand.java
@@ -14,18 +14,23 @@ abstract class MinecraftLinkCommand {
 	static boolean execute(CommandSender sender, Command command, String alias, String[] args) {
 		if(sender instanceof Player) {
 			if(args.length > 0) {
-				final User user = DiscordBot.jda.getUserByTag(String.join(" ", args));
+				try {
+					final User user = DiscordBot.jda.getUserByTag(String.join(" ", args));
 
-				if(user == null) {
+					if(user == null) {
+						sender.sendMessage(ChatColor.GOLD + "Invalid Discord tag!");
+						return true;
+					}
+
+					if(AccountLinking.beginLinking(((Player) sender).getUniqueId(), user.getId())) {
+						user.openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage("Did you just begin linking your Discord and Minecraft accounts?\nReply to this with the username of your Minecraft account to complete linking!\n(Note: You must be currently logged onto the server to confirm the account linking.)").queue());
+						sender.sendMessage(ChatColor.GREEN + "Confirmation message sent to discord!");
+					} else {
+						sender.sendMessage(ChatColor.GOLD + "Accounts already linked!");
+					}
+				} catch(IllegalArgumentException exc) {
 					sender.sendMessage(ChatColor.GOLD + "Invalid Discord tag!");
 					return true;
-				}
-
-				if(AccountLinking.beginLinking(((Player) sender).getUniqueId(), user.getId())) {
-					user.openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage("Did you just begin linking your Discord and Minecraft accounts?\nReply to this with the username of your Minecraft account to complete linking!\n(Note: You must be currently logged onto the server to confirm the account linking.)").queue());
-					sender.sendMessage(ChatColor.GREEN + "Confirmation message sent to discord!");
-				} else {
-					sender.sendMessage(ChatColor.GOLD + "Accounts already linked!");
 				}
 			} else {
 				return false;
